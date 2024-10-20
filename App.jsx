a353f8ed59171a34fa3f56024f364906a406fa8b
sriram N V <sriramnvilvanathan@gmail.com>
2024-10-20 13:09:59 +0530

    fixed drag and drop bug

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
-import NotesPage from './pages/NotesPage';
-import NotesProvider from './context/NotesContext';
-
+import NotesPage from "./pages/NotesPage";
+import NotesProvider from "./context/NotesContext";
 
 function App() {
-  return (<div id='app'>
-    <NotesProvider>
-      <NotesPage />
-    </NotesProvider>
-  </div>)
-
+    return (
+        <div id="app">
+            <NotesProvider>
+                <NotesPage />
+            </NotesProvider>
+        </div>
+    );
 }
 
-export default App
+export default App;
