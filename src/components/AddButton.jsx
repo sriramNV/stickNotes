eaea97caa394dc295c08278e574b1a1fb6de0a81
sriram N V <sriramnvilvanathan@gmail.com>
2024-10-20 13:05:38 +0530

    fixed add  button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,12 +1,13 @@
-import Plus from '../icons/Plus';
-import colors from '../assets/colors.json';
-import { NotesContext } from '../context/NotesContext';
-import { useContext, useRef } from 'react';
-import { db } from '../appwrite/database';
+import React from "react";
+import Plus from "../icons/Plus";
+import colors from "../assets/colors.json";
+import { useRef } from "react";
+import { db } from "../appwrite/database";
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
 
 const AddButton = () => {
     const { setNotes } = useContext(NotesContext);
-
     const startingPos = useRef(10);
 
     const addNote = async () => {
@@ -17,18 +18,18 @@ const AddButton = () => {
             }),
             colors: JSON.stringify(colors[0]),
         };
+
         startingPos.current += 10;
 
         const response = await db.notes.create(payload);
         setNotes((prevState) => [response, ...prevState]);
-    }
+    };
 
-
-    return(
-        <div id='add-button'>
+    return (
+        <div id="add-btn" onClick={addNote}>
             <Plus />
         </div>
     );
 };
 
-export  default AddButton
+export default AddButton;
