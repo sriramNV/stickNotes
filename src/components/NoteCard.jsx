fa0430598b1d723a7eef51b3745631147092eaaa
sriram N V <sriramnvilvanathan@gmail.com>
2024-10-20 11:26:26 +0530

    added note cards

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,6 +1,47 @@
 const NoteCard = ({ note }) => {
+    let position = JSON.parse(note.position);
+    const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
-    return <div>{body}</div>
+    
+    const textAreaRef = useRef(null);
+
+    useEffect(() => {
+        autoGrow(textAreaRef);
+    }, []);
+
+    function autoGrow(textAreaRef) {
+        const { current } = textAreaRef;
+        current.style.height = "auto"; // Reset the height
+        current.style.height = current.scrollHeight + "px"; // Set the new height
+    }
+
+    
+    return(
+        <div 
+            className="card" 
+            style={{backgroundColor: colors.colorBody,
+                left: `${position.x}px`,
+                top: `${position.y}px`, 
+            }}>
+                <div 
+                    className="card-header"
+                    style={{backgroundColor: colors.colorHeader}}>
+                    <Trash />
+                </div>    
+                
+
+                <div className="card-body">
+                    <textarea 
+                        ref={textAreaRef}
+                        style={{color: colors.colorText}}
+                        defaultValue={body}>
+                        onInput={() => {
+                            autoGrow(textAreaRef);
+                        }}
+                    </textarea>
+                </div>      
+            </div>
+    );
 };
 
 export default NoteCard;
